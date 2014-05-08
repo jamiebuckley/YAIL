bbda0b720e8a7aa16c35f24bede04ca70106161c
Jamie Buckley <jbuckley760@gmail.com>
2014-05-08 20:42:40 +0100

    Added HashMap funcionality (Needs work and tidying)

---
+++ HashMap.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "HashMap.h"
+
+/*
+int main(int argc, char** argv)
+{
+	char* arrayofstrings[] = {"Hello", "this", "is", "a", "simple", "hash", "map"};
+	char* arrayofdata[] = {"Goodbye", "that", "isn't", "several", "complex", "perfect", "compass"};
+
+	HashMap* map = newHashMap(128);
+
+	for(int i = 0; i < 7; i++)
+	{
+		hashMap_put(map, arrayofstrings[i], arrayofdata[i]);	
+	}
+	char* val = (char*)hashMap_get(map, "Hello")->data;
+	printf("retrieved %s from hashmap\n", val);
+
+	int num = hashMap_remove(map, "Hello", NULL);
+	void* data = hashMap_get(map, "Hello");
+	if(data == NULL)
+	{
+		printf("Success!\n");
+	}
+}
+*/
+
+HashMap* newHashMap(int size)
+{
+	if(size==0 || (size & (size - 1)))
+	{
+		printf("Error, invalid size\n");
+		return NULL;	
+	}
+
+	HashMap* result = malloc(sizeof(HashMap));
+
+	result->numOfBuckets = size;
+	result->buckets = calloc(size, sizeof(dlinklist));
+
+}
+
+int hashMap_hash(char* data, int size)
+{
+	int result = 0;
+	char* current = data;
+	while(current!= NULL && *current != 0)
+	{
+		result+=*current;
+		result<<2;
+		result = result & (size-1);
+		current++;
+	}
+	return result;
+}
+
+int hashMap_put(HashMap* hashMap, char* value, void* data)
+{
+	int bucketIndex = hashMap_hash(value, hashMap->numOfBuckets);
+	printf("Inserting %s into bucket index %d with value %s\n", (char*)data, bucketIndex, value);
+	HashMapEntry* entry = malloc(sizeof(HashMapEntry));
+	entry->value = value;
+	entry->data = data;
+	append(hashMap->buckets+bucketIndex, entry);
+}
+
+HashMapEntry* hashMap_get(HashMap* hashMap, char* data)
+{
+	int bucketIndex = hashMap_hash(data, hashMap->numOfBuckets);
+	
+	node* current = (hashMap->buckets+bucketIndex)->start;
+
+	while(current != NULL)
+	{
+		HashMapEntry* thisEntry = (HashMapEntry*)(current->data);
+
+		if(!strcmp(thisEntry->value, data))
+		{
+			return thisEntry;
+		}
+		else
+		{
+			current=current->next;
+		}
+	}
+	return NULL;
+}
+
+int hashMapCompare(node* thisNode, void* data)
+{
+	HashMapEntry* entry = thisNode->data;
+	return !strcmp(entry->value, (char*)data); 
+}
+
+int hashMap_remove(HashMap* hashMap, char* data, nodeOp f)
+{
+	int bucketIndex = hashMap_hash(data, hashMap->numOfBuckets);
+	dlinklist* thislist = hashMap->buckets+bucketIndex;
+	return list_remove_comparator(thislist, data, f, hashMapCompare, data);	
+}
---
+++ HashMap.h
@@ -0,0 +1,24 @@
+#ifndef HASHMAP_HEADER
+#define HASHMAP_HEADER
+
+#include "List.h"
+
+typedef struct HashMapEntry
+{
+	char* value;
+	void* data;	
+} HashMapEntry;
+
+typedef struct HashMap
+{
+	int numOfBuckets;
+	dlinklist* buckets;
+} HashMap;
+
+HashMap* newHashMap(int size);
+int hashMap_hash(char* data, int size);
+int hashMap_put(HashMap*, char* value, void* data);
+HashMapEntry* hashMap_get(HashMap*, char* data);
+int hashMap_remove(HashMap*, char* data, nodeOp f);
+
+#endif
